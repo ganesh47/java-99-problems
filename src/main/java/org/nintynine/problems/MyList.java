5088e039f4001343d1cbe7c51dcabea201f12c71
ganesh47 <22994026+ganesh47@users.noreply.github.com>
2025-05-11 13:47:49 +0000

    java-99-problems-1 P01 Find the last box of a list
    Signed-off-by: ganesh47 <22994026+ganesh47@users.noreply.github.com>

---
+++ src/main/java/org/nintynine/problems/MyList.java
@@ -0,0 +1,24 @@
+package org.nintynine.problems;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class MyList<T> {
+    private final T[] items;
+
+    @SafeVarargs
+    public MyList(T... elements) {
+        this.items = Arrays.copyOf(elements, elements.length);
+    }
+    //P01 : Find the last box of a list
+    public T last() {
+        return Arrays.stream(items, 0, items.length)
+                .reduce((_, b) -> b)
+                .orElseThrow(() -> new IllegalStateException("Empty list has no last element"));
+    }
+
+    @Override
+    public String toString() {
+        return "(" + String.join(" ", Arrays.stream(items).map(Objects::toString).toList()) + ")";
+    }
+}
