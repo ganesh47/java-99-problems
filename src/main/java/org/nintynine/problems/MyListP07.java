27a44ea67a4643d23f7d6e543badfffbb7f1fbf0
ganesh47 <22994026+ganesh47@users.noreply.github.com>
2025-05-12 04:55:47 +0000

    [#7] P07 (**) Flatten a nested list structure.
    Signed-off-by: ganesh47 <22994026+ganesh47@users.noreply.github.com>

---
+++ src/main/java/org/nintynine/problems/MyListP07.java
@@ -0,0 +1,29 @@
+package org.nintynine.problems;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class MyListP07<T> extends MyListP06<T> {
+    @SafeVarargs
+    public MyListP07(T... elements) {
+        super(elements);
+    }
+    
+    @SuppressWarnings("unchecked")
+    public MyListP07<T> flatten() {
+        T[] flattenedArray = (T[]) Arrays.stream(items)
+                .flatMap(this::flattenHelper)
+                .toArray();
+        return new MyListP07<>(flattenedArray);
+    }
+    
+    private Stream<?> flattenHelper(Object item) {
+        return switch (item) {
+            case MyListP07<?> myListP07 -> Arrays.stream(myListP07.items).flatMap(this::flattenHelper);
+            case List<?> objects -> objects.stream().flatMap(this::flattenHelper);
+            case Object[] objects -> Arrays.stream(objects).flatMap(this::flattenHelper);
+            case null, default -> Stream.of(item);
+        };
+    }
+}
