0d39b75905ff260ec4e368b89c15dd96c5596b95
ganesh47 <22994026+ganesh47@users.noreply.github.com>
2025-05-12 03:43:35 +0000

    [java-99-problems-2] P02 Find the last but one box of a list.

---
+++ src/main/java/org/nintynine/problems/MyListP02.java
@@ -0,0 +1,33 @@
+package org.nintynine.problems;
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+public class MyListP02<T> extends MyList<T> {
+    @SafeVarargs
+    public MyListP02(T... elements) {
+        super(elements);
+    }
+    private record Pair<T>(T first, T second) {
+        static <T> Pair<T> empty() {
+            return new Pair<>(null, null);
+        }
+
+        static <T> Pair<T> shift(Pair<T> prev, T next) {
+            return new Pair<>(prev.second, next);
+        }
+
+        T secondLastOrThrow() {
+            if (first == null) {
+                throw new NoSuchElementException("List has fewer than two elements");
+            }
+            return first;
+        }
+    }
+
+    public T lastButOne() {
+        return Arrays.stream(items)
+                .reduce(Pair.<T>empty(), Pair::shift, (a, b) -> b)
+                .secondLastOrThrow();
+    }
+    }
